8f4e63d1fcff56049a33c0551313b807675940ab
Dan <40447572+test-dan-run@users.noreply.github.com>
2020-09-08 18:29:31 +0800

    add debug

--- train.py
+++ train.py
@@ -612,6 +612,8 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
 
+    print(cfg)
+    
     if cfg.use_darknet_cfg:
         model = Darknet(cfg.cfgfile)
     else:
